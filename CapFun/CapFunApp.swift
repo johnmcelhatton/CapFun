7ed9c90723d90d2b57d41c90252cf4c5f7ef5714
John McElhatton <jmcelhatton2010@gmail.com>
2024-01-23 15:36:25 -0500

    Initial Commit

---
+++ CapFun/CapFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapFunApp.swift
+//  CapFun
+//
+//  Created by John McElhatton on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CapFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
